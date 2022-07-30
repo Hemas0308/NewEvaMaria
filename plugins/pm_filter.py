fa4fd3c07b4424e1a0dde28f928cc35cd6d43ba7
RAJANGAM R <85456269+RAJANGAMR@users.noreply.github.com>
2022-07-30 07:46:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -794,7 +794,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist)) # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+        k = await msg.reply("நீங்கள் தேடும் நூலின் பெயர் அல்லது ஆசிரியர் பெயரை மட்டும் எழுதுங்கள்.")
         await asyncio.sleep(8)
         await k.delete()
         return
